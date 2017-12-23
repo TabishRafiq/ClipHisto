f3e9db9d1e00ef175c3f2efbd5e85426d0d2eb76
Tabish Rafiq <tabish_rafiq45@hotmail.com>
2017-12-23 23:18:50 +0500

    Add project files.

---
+++ ClipHisto/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace ClipHisto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.startWithPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ClipHisto";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.startWithPCToolStripMenuItem,
+            this.historyToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(143, 104);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(139, 6);
+            // 
+            // startWithPCToolStripMenuItem
+            // 
+            this.startWithPCToolStripMenuItem.Checked = true;
+            this.startWithPCToolStripMenuItem.CheckOnClick = true;
+            this.startWithPCToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.startWithPCToolStripMenuItem.Name = "startWithPCToolStripMenuItem";
+            this.startWithPCToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.startWithPCToolStripMenuItem.Text = "Start with PC";
+            this.startWithPCToolStripMenuItem.CheckedChanged += new System.EventHandler(this.startWithPCToolStripMenuItem_CheckedChanged);
+            // 
+            // historyToolStripMenuItem
+            // 
+            this.historyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemsToolStripMenuItem,
+            this.itemsToolStripMenuItem1,
+            this.itemsToolStripMenuItem2,
+            this.itemsToolStripMenuItem3,
+            this.itemsToolStripMenuItem4});
+            this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.historyToolStripMenuItem.Text = "History ";
+            // 
+            // itemsToolStripMenuItem
+            // 
+            this.itemsToolStripMenuItem.CheckOnClick = true;
+            this.itemsToolStripMenuItem.Name = "itemsToolStripMenuItem";
+            this.itemsToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.itemsToolStripMenuItem.Text = "10 items";
+            this.itemsToolStripMenuItem.Click += new System.EventHandler(this.itemsToolStripMenuItem_Click);
+            // 
+            // itemsToolStripMenuItem1
+            // 
+            this.itemsToolStripMenuItem1.CheckOnClick = true;
+            this.itemsToolStripMenuItem1.Name = "itemsToolStripMenuItem1";
+            this.itemsToolStripMenuItem1.Size = new System.Drawing.Size(118, 22);
+            this.itemsToolStripMenuItem1.Text = "20 items";
+            this.itemsToolStripMenuItem1.Click += new System.EventHandler(this.itemsToolStripMenuItem1_Click);
+            // 
+            // itemsToolStripMenuItem2
+            // 
+            this.itemsToolStripMenuItem2.CheckOnClick = true;
+            this.itemsToolStripMenuItem2.Name = "itemsToolStripMenuItem2";
+            this.itemsToolStripMenuItem2.Size = new System.Drawing.Size(118, 22);
+            this.itemsToolStripMenuItem2.Text = "30 items";
+            this.itemsToolStripMenuItem2.Click += new System.EventHandler(this.itemsToolStripMenuItem2_Click);
+            // 
+            // itemsToolStripMenuItem3
+            // 
+            this.itemsToolStripMenuItem3.CheckOnClick = true;
+            this.itemsToolStripMenuItem3.Name = "itemsToolStripMenuItem3";
+            this.itemsToolStripMenuItem3.Size = new System.Drawing.Size(118, 22);
+            this.itemsToolStripMenuItem3.Text = "40 items";
+            this.itemsToolStripMenuItem3.Click += new System.EventHandler(this.itemsToolStripMenuItem3_Click);
+            // 
+            // itemsToolStripMenuItem4
+            // 
+            this.itemsToolStripMenuItem4.CheckOnClick = true;
+            this.itemsToolStripMenuItem4.Name = "itemsToolStripMenuItem4";
+            this.itemsToolStripMenuItem4.Size = new System.Drawing.Size(118, 22);
+            this.itemsToolStripMenuItem4.Text = "50 items";
+            this.itemsToolStripMenuItem4.Click += new System.EventHandler(this.itemsToolStripMenuItem4_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(139, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(16, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(263, 320);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Double click to copy";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(264, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(18, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.SteelBlue;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.DodgerBlue;
+            this.linkLabel1.Location = new System.Drawing.Point(227, 371);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(51, 15);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Clear All";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(14, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ClipHisto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(295, 395);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1a";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem startWithPCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ClipHisto/Form1.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipHisto
+{
+    public partial class Form1 : Form
+    {
+        
+        string lastData;
+        public Form1()
+        {
+             this.Visible = false;
+            InitializeComponent();
+            
+        }
+        private void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (ClipHisto.Properties.Settings.Default.start_with_pc == true)
+            rk.SetValue("ClipHisto", Application.ExecutablePath.ToString());
+            else
+            rk.DeleteValue("ClipHisto", false);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // base.Hide();
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height-40);
+            SetStartup();
+            unCheckAll();
+            if (Properties.Settings.Default.history_level == 10)
+                itemsToolStripMenuItem.Checked = true;
+            else if (Properties.Settings.Default.history_level == 20)
+                itemsToolStripMenuItem1.Checked = true;
+            else if (Properties.Settings.Default.history_level == 30)
+                itemsToolStripMenuItem2.Checked = true;
+            else if (Properties.Settings.Default.history_level == 40)
+                itemsToolStripMenuItem3.Checked = true;
+            else
+                itemsToolStripMenuItem4.Checked = true;
+
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+                return;
+            var rect1 = new Rectangle(e.Bounds.X, e.Bounds.Y, listBox1.Width, 32);
+            var rect = new Rectangle(e.Bounds.X, e.Bounds.Y, 20, 20);
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                e = new DrawItemEventArgs(e.Graphics,
+                                          e.Font, rect1,
+                                          e.Index,
+                                          e.State ^ DrawItemState.Selected,
+                                          Color.White,
+                                          Color.FromArgb(255, 217, 236, 252)); //Choose the color
+            Brush databr = new SolidBrush(Color.FromArgb(255, 5, 5, 5));
+            Brush timebr = new SolidBrush(Color.FromArgb(255, 9, 136, 239));
+
+            e.DrawBackground();
+            var co = (clipObject)listBox1.Items[e.Index];
+            Font n = new Font("Segoe UI", 7.5f, FontStyle.Italic);
+            e.Graphics.DrawString(textLimiter(co.data,42), e.Font, databr, e.Bounds.X + 5, e.Bounds.Y, StringFormat.GenericDefault);
+            e.Graphics.DrawString(co.time, n, timebr, e.Bounds.X + 5, e.Bounds.Y + 17, StringFormat.GenericDefault);
+            databr.Dispose();
+            e.Graphics.Flush();
+            e.DrawFocusRectangle();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            string currentData = Clipboard.GetText();
+           
+            if (currentData != lastData)
+            {
+                listBox1.BeginUpdate();
+
+                clipObject co = new clipObject();
+                co.data = currentData;
+                co.time = DateTime.Now.ToLongTimeString();
+                listBox1.Items.Insert(0, co);
+                lastData = currentData;
+
+                if (listBox1.Items.Count > Properties.Settings.Default.history_level)
+                    listBox1.Items.RemoveAt(Properties.Settings.Default.history_level - 1);
+
+                listBox1.EndUpdate();
+            }
+          
+        }
+
+        public string textLimiter(string source, int length)
+        {
+            string final;
+            final = source.Split('\n')[0];
+            if (source.Length > length)
+            {
+                try
+                {
+                    final = final.Substring(0, length);
+                    final += "...";
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return final;
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var op = (clipObject)listBox1.Items[listBox1.SelectedIndex];
+            Clipboard.SetText(op.data);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void startWithPCToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            ClipHisto.Properties.Settings.Default.start_with_pc = startWithPCToolStripMenuItem.Checked;
+            ClipHisto.Properties.Settings.Default.Save();
+        }
+        private void itemsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            unCheckAll();
+            itemsToolStripMenuItem.Checked = true;
+            Properties.Settings.Default.history_level = 10;
+            Properties.Settings.Default.Save();
+        }
+
+        private void unCheckAll()
+        {
+            itemsToolStripMenuItem.Checked = false;
+            itemsToolStripMenuItem1.Checked = false;
+            itemsToolStripMenuItem2.Checked = false;
+            itemsToolStripMenuItem3.Checked = false;
+            itemsToolStripMenuItem4.Checked = false;    
+        }
+        private void itemsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            unCheckAll();
+            itemsToolStripMenuItem1.Checked = true;
+            Properties.Settings.Default.history_level = 20;
+            Properties.Settings.Default.Save();
+        }
+
+        private void itemsToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            unCheckAll();
+            itemsToolStripMenuItem2.Checked = true;
+            Properties.Settings.Default.history_level = 30;
+            Properties.Settings.Default.Save();
+        }
+
+        private void itemsToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            unCheckAll();
+            itemsToolStripMenuItem3.Checked = true;
+            Properties.Settings.Default.history_level = 40;
+            Properties.Settings.Default.Save();
+        }
+
+        private void itemsToolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            unCheckAll();
+            itemsToolStripMenuItem4.Checked = true;
+            Properties.Settings.Default.history_level = 50;
+            Properties.Settings.Default.Save();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.ClientRectangle, Color.Silver, ButtonBorderStyle.Solid);
+        }
+    }
+    public class clipObject
+        {
+        public string data;
+        public string time;
+        }
+
+}
